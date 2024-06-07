fa32fcacdb28f9a98ca9d8c88f1435c014c5bb9d
Ibai Farina <ibaifo8@gmail.com>
2024-06-07 17:02:16 +0200

    seo

--- app/layout.js
+++ app/layout.js
@@ -12,6 +12,26 @@ export const metadata = {
 export default function RootLayout({children}) {
     return (
         <html lang="es">
+        <head>
+            <meta name="description" content="Glosario de términos gramaticales para las pruebas PAU."/>
+            <meta name="keywords"
+                  content="glosario, gramática, RAE, selectividad, PAU, evau, glosario términos gramaticales, términos, lengua, castellano, bachillerato"/>
+            <meta name="robots" content="index, follow"/>
+            <meta charSet="UTF-8"/>
+
+            {/* Open Graph Meta Tags for Social Media */}
+            <meta property="og:title" content="Glosario de términos gramaticales"/>
+            <meta property="og:description" content="Glosario de términos gramaticales para las pruebas PAU."/>
+            <meta property="og:type" content="website"/>
+
+            {/* Twitter Card Meta Tags */}
+            <meta name="twitter:card" content="summary_large_image"/>
+            <meta name="twitter:title" content="Glosario de términos gramaticales"/>
+            <meta name="twitter:description" content="Glosario de términos gramaticales para las pruebas PAU."/>
+
+            <meta name="google-site-verification" content="RM97TIqIjBFTrckKsIZgr9BDJ7xA8P_rYJfpByf6J2c"/>
+            <title>Glosario de términos gramaticales</title>
+        </head>
         <body className={inter.className}>
         <ProductFilterProvider>
             {children}
